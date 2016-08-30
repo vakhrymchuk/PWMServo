32ba14bb2a1df48b4e10e25ae0f5d7c82b28609f
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2016-08-30 11:15:59 +0300

    Code reformat

--- PWMServo.h
+++ PWMServo.h
@@ -53,32 +53,41 @@
   #define SERVO_PIN_B 10
 #endif
 
-class PWMServo
-{
-  private:
+class PWMServo {
+private:
     uint8_t pin;
     uint8_t angle;       // in degrees
     uint8_t min16;       // minimum pulse, 16uS units  (default is 34)
     uint8_t max16;       // maximum pulse, 16uS units, 0-4ms range (default is 150)
+
     static void seizeTimer1();
+
     static void releaseTimer1();
+
     static uint8_t attachedA;
     static uint8_t attachedB;
-    #ifdef SERVO_PIN_C
+#ifdef SERVO_PIN_C
     static uint8_t attachedC;
-    #endif
-  public:
+#endif
+public:
+
     PWMServo();
+
     uint8_t attach(int);
-                             // pulse length for 0 degrees in microseconds, 544uS default
-                             // pulse length for 180 degrees in microseconds, 2400uS default
+
+    // pulse length for 0 degrees in microseconds, 544uS default
+    // pulse length for 180 degrees in microseconds, 2400uS default
     uint8_t attach(int, int, int);
-                             // attach to a pin, sets pinMode, returns 0 on failure, won't
-                             // position the servo until a subsequent write() happens
-                             // Only works for 9 and 10.
+
+    // attach to a pin, sets pinMode, returns 0 on failure, won't
+    // position the servo until a subsequent write() happens
+    // Only works for 9 and 10.
     void detach();
+
     void write(int);         // specify the angle in degrees, 0 to 180
+
     uint8_t read();
+
     uint8_t attached();
 };
 
